0d10e146c4e6e5c5d5ad194ceafd062d2d8fcab8
simalbutt <138391970+simalbutt@users.noreply.github.com>
2025-12-22 12:06:05 +0500

    created product model

---
+++ product/models/__init__.py
@@ -0,0 +1,6 @@
+from .catagory import Category
+from .product import Product
+from .feature import ProductFeature
+from .variant import ProductVariant
+from .image import ProductImage, VariantImage
+from .vendor import Vendor
